6e2765ab53639c9e46c363d89f50939ad7c0f269
kcrampon <kevin.crampon@univ-reims.fr>
2023-11-17 09:37:31 +0100

    Change version number un setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SGPocket',
-    version='1.0',
+    version='1.0.0',
     description='A Graph Convolutional Neural Network to predict ligand binding site on a protein.',
     author='Kevin Crampon',
     author_email='kevin.crampon@univ-reims.fr',
